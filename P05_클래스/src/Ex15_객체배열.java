7cd4b8ed9aa329abee4184cf704389db6a309c66
bkoon <bkoon@DESKTOP-TR7VRF3>
2025-08-19 10:08:23 +0900

    Ex15 commit

---
+++ P05_클래스/src/Ex15_객체배열.java
@@ -0,0 +1,19 @@
+class Book{
+	private String title;
+	private int price;
+	
+	Book () {
+		title="없음";
+		price=0;
+	}
+}
+
+public class Ex15_객체배열 {
+	public static void main(String[] args) {
+
+		Book[] bk1 = {new Book(),new Book(),new Book()};
+		Book[] bk2 = new Book[3];
+		
+	}
+
+}
